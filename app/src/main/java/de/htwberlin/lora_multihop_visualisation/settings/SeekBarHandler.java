4fdb52098a677475aee7671c0f4bafdc04848f5c
djagya <danil.kabluk@gmail.com>
2019-05-14 10:35:45 +0200

    fixed the bug with crashing on last bar position

--- app/src/main/java/de/htwberlin/lora_multihop_visualisation/settings/SeekBarHandler.java
+++ app/src/main/java/de/htwberlin/lora_multihop_visualisation/settings/SeekBarHandler.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-// todo: it crashes on moving to the end of the bar, probably some issue with out of bound index
 public class SeekBarHandler {
     private SeekBar input;
     private TextView textView;
@@ -105,6 +104,4 @@ public class SeekBarHandler {
 
         }
     }
-
-
 }
