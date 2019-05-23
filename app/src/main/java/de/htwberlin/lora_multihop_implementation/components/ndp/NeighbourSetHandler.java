508002dd081ab38ab1362af0c4437b5de7d98f82
peteme <p.meindl97@gmail.com>
2019-05-23 20:22:19 +0200

    add neighbour set handler to NDP package --> used for creating & storing db in attribute

---
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/ndp/NeighbourSetHandler.java
@@ -0,0 +1,22 @@
+package de.htwberlin.lora_multihop_implementation.components.ndp;
+
+import android.support.v7.app.AppCompatActivity;
+import androidx.room.Room;
+import de.htwberlin.lora_multihop_implementation.components.storage.NeighbourSetDatabase;
+
+public class NeighbourSetHandler extends AppCompatActivity {
+
+	private NeighbourSetDatabase db;
+
+	public NeighbourSetHandler()	{
+		this.db = Room.databaseBuilder(getApplicationContext(), NeighbourSetDatabase.class, "neighbour-sets-db").build();
+	}
+
+	public NeighbourSetDatabase getDb() {
+		return db;
+	}
+
+	public void setDb(NeighbourSetDatabase db) {
+		this.db = db;
+	}
+}
