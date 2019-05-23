6511aa43316634cf838fe57e278c82bda5bab7e4
peteme <p.meindl97@gmail.com>
2019-05-23 19:53:05 +0200

    added override methods

--- app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/DBNeighbourSetDAO.java
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/DBNeighbourSetDAO.java
@@ -1,4 +1,39 @@
 package de.htwberlin.lora_multihop_implementation.components.storage;
 
+import de.htwberlin.lora_multihop_implementation.components.model.NeighbourSet;
+
+import java.util.List;
+
 public class DBNeighbourSetDAO implements NeighbourSetDAO {
+
+	//
+
+	public DBNeighbourSetDAO() {
+		//
+	}
+
+	@Override
+	public NeighbourSet getNeighbourSetByUid(int uid) {
+		return null;
+	}
+
+	@Override
+	public List<NeighbourSet> getAllNeighbourSets() {
+		return null;
+	}
+
+	@Override
+	public int saveNeighbourSet(NeighbourSet song) {
+		return 0;
+	}
+
+	@Override
+	public boolean updateNeighbourSet(NeighbourSet song) {
+		return false;
+	}
+
+	@Override
+	public boolean deleteNeighbourSet(int uid) {
+		return false;
+	}
 }
