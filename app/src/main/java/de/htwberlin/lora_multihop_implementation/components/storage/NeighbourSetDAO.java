b2d3556d12a821bcdbe8f16f7d516bae99ddf866
peteme <p.meindl97@gmail.com>
2019-05-23 19:47:04 +0200

    fill NeighbourSetDao with basic methods

--- app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/NeighbourSetDAO.java
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/NeighbourSetDAO.java
@@ -1,4 +1,18 @@
 package de.htwberlin.lora_multihop_implementation.components.storage;
 
+import de.htwberlin.lora_multihop_implementation.components.model.NeighbourSet;
+
+import java.util.List;
+
 public interface NeighbourSetDAO {
+
+	NeighbourSet getNeighbourSetByUid(int uid);
+
+	List<NeighbourSet> getAllNeighbourSets();
+
+	int saveNeighbourSet(NeighbourSet song);
+
+	boolean updateNeighbourSet(NeighbourSet song);
+
+	boolean deleteNeighbourSet(int uid);
 }
