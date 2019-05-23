d6a47ab8ba3b0889812d599ddc00c0ef0d0d855c
peteme <p.meindl97@gmail.com>
2019-05-23 20:21:14 +0200

    add neighbour set database class which provides access to dao for queries

---
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/NeighbourSetDatabase.java
@@ -0,0 +1,10 @@
+package de.htwberlin.lora_multihop_implementation.components.storage;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import de.htwberlin.lora_multihop_implementation.components.model.NeighbourSet;
+
+@Database(entities = {NeighbourSet.class}, version = 1)
+public abstract class NeighbourSetDatabase extends RoomDatabase {
+	public abstract NeighbourSetDAO neighbourSetDao();
+}
