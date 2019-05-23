27149a579cc0e583245af8e30603677ff8b619b2
peteme <p.meindl97@gmail.com>
2019-05-23 20:18:30 +0200

    added room annotations (queries) to neighbour set dao

--- app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/NeighbourSetDAO.java
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/storage/NeighbourSetDAO.java
@@ -1,18 +1,25 @@
 package de.htwberlin.lora_multihop_implementation.components.storage;
 
+import androidx.room.*;
 import de.htwberlin.lora_multihop_implementation.components.model.NeighbourSet;
 
 import java.util.List;
 
+@Dao
 public interface NeighbourSetDAO {
 
+	@Query("SELECT * FROM neighbourset WHERE uid LIKE :uid LIMIT 1")
 	NeighbourSet getNeighbourSetByUid(int uid);
 
+	@Query("SELECT * FROM neighbourset")
 	List<NeighbourSet> getAllNeighbourSets();
 
+	@Insert
 	int saveNeighbourSet(NeighbourSet song);
 
+	@Update
 	boolean updateNeighbourSet(NeighbourSet song);
 
+	@Delete
 	boolean deleteNeighbourSet(int uid);
 }
