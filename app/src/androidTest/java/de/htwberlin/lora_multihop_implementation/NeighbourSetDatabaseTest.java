78f836720c8b132afc8b3faefdab64052173f727
peteme <p.meindl97@gmail.com>
2019-05-25 18:46:15 +0200

    addede basic test case add test entry

--- app/src/androidTest/java/de/htwberlin/lora_multihop_implementation/NeighbourSetDatabaseTest.java
+++ app/src/androidTest/java/de/htwberlin/lora_multihop_implementation/NeighbourSetDatabaseTest.java
@@ -8,6 +8,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.app.AppCompatActivity;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +16,7 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.List;
 
 import de.htwberlin.lora_multihop_implementation.components.model.NeighbourSet;
 import de.htwberlin.lora_multihop_implementation.components.storage.NeighbourSetDAO;
@@ -24,19 +26,22 @@ import de.htwberlin.lora_multihop_implementation.enums.ELoraNodeState;
 @RunWith(AndroidJUnit4.class)
 public class NeighbourSetDatabaseTest {
 
-    private NeighbourSetDAO userDao;
+    private NeighbourSetDAO neighbourSetDAO;
     private NeighbourSetDatabase db;
 
     @Before
     public void setUp() throws Exception {
         Context context = InstrumentationRegistry.getTargetContext();
         db = Room.inMemoryDatabaseBuilder(context, NeighbourSetDatabase.class).build();
-        userDao = db.neighbourSetDao();
+        neighbourSetDAO = db.neighbourSetDao();
     }
 
     @Test
-    public void test() {
+    public void testAddDbEntry() {
+        neighbourSetDAO.saveNeighbourSet(createNeighbourSet());
+        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
 
+        Assert.assertEquals(1, neighbourSets.size());
     }
 
     @After
