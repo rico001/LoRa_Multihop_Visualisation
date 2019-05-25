23feec4571fa3bfed24d264d7a1a58a893e68f31
djagya <danil.kabluk@gmail.com>
2019-05-25 21:22:42 +0200

    finished settings page

--- app/src/main/java/de/htwberlin/lora_multihop_visualisation/LoraSettingsActivity.java
+++ app/src/main/java/de/htwberlin/lora_multihop_visualisation/LoraSettingsActivity.java
@@ -3,37 +3,17 @@ package de.htwberlin.lora_multihop_visualisation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import de.htwberlin.lora_multihop_implementation.components.lora.LoRaConfig;
 import de.htwberlin.lora_multihop_visualisation.settings.SeekBarHandler;
+import de.htwberlin.lora_multihop_visualisation.settings.SwitchHandler;
 
 /**
  * configuration for some AT CMDS (e.g. CONFIGURE_CMD).
  */
 public class LoraSettingsActivity extends AppCompatActivity {
     LoRaConfig loRaConfig;
-
-    //SeekBars
-    SeekBar seekBar_transmitPower;
-    SeekBar seekBar_frequency;
-    SeekBar seekBar_bandwidth;
-    SeekBar seekBar_spreading;
-    SeekBar seekBar_errorCoding;
-
-    //TextViews
-    TextView textView_transmitPower;
-    TextView textView_frequency;
-    TextView textView_bandwidth;
-    TextView textView_spreading;
-    TextView textView_errorCoding;
-    TextView textView_CRC;
-
-    //Switches
-    Switch switch_crc;
 
     Button button_saveSettings;
 
@@ -45,10 +25,8 @@ public class LoraSettingsActivity extends AppCompatActivity {
         LoRaApplication app = (LoRaApplication) getApplicationContext();
         loRaConfig = app.getLoRaConfig();
 
-        initTextViews();
         initSeekBars();
         initSwitches();
-        syncTextViewsAndSwitches();
 
         // Save button.
         button_saveSettings = findViewById(R.id.button_saveSettings);
@@ -59,47 +37,71 @@ public class LoraSettingsActivity extends AppCompatActivity {
         });
     }
 
-    private void initTextViews() {
-        textView_transmitPower = findViewById(R.id.textView_transmitPower);
-        textView_frequency = findViewById(R.id.textView_frequency);
-        textView_bandwidth = findViewById(R.id.textView_bandwidth);
-        textView_spreading = findViewById(R.id.textView_spreading);
-        textView_errorCoding = findViewById(R.id.textView_errorCoding);
-
-        textView_CRC = findViewById(R.id.textView_CRC);
-    }
-
     private void initSeekBars() {
-        seekBar_frequency = findViewById(R.id.seekBar_frequency);
-        new SeekBarHandler(seekBar_frequency, textView_frequency, 410, 470, "MHz")
-                .setValueProxy(() -> loRaConfig.getFrequency(), (v) -> loRaConfig.setFrequency(v));
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_frequency),
+                findViewById(R.id.textView_frequency),
+                410, 470, "MHz"
+        ).setValueProxy(() -> loRaConfig.getFrequency(), (v) -> loRaConfig.setFrequency(v));
 
-        seekBar_transmitPower = findViewById(R.id.seekBar_transmitPower);
-        new SeekBarHandler(seekBar_transmitPower, textView_transmitPower, 5, 20, "dBm")
-                .setValueProxy(() -> loRaConfig.getPower(), (v) -> loRaConfig.setPower(v));
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_transmitPower),
+                findViewById(R.id.textView_transmitPower),
+                5, 20, "dBm"
+        ).setValueProxy(() -> loRaConfig.getPower(), (v) -> loRaConfig.setPower(v));
 
-        seekBar_bandwidth = findViewById(R.id.seekBar_bandwidth);
-        new SeekBarHandler(seekBar_bandwidth, textView_bandwidth, LoRaConfig.bandwidthMap)
-                .setValueProxy(() -> loRaConfig.getSignalBw(), (v) -> loRaConfig.setSignalBw(v));
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_bandwidth),
+                findViewById(R.id.textView_bandwidth),
+                LoRaConfig.bandwidthMap
+        ).setValueProxy(() -> loRaConfig.getSignalBw(), (v) -> loRaConfig.setSignalBw(v));
 
-        seekBar_spreading = findViewById(R.id.seekBar_spreading);
-        new SeekBarHandler(seekBar_spreading, textView_spreading, LoRaConfig.spreadingMap)
-                .setValueProxy(() -> loRaConfig.getSpreadingFactor(), (v) -> loRaConfig.setSpreadingFactor(v));
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_spreading),
+                findViewById(R.id.textView_spreading),
+                LoRaConfig.spreadingMap
+        ).setValueProxy(() -> loRaConfig.getSpreadingFactor(), (v) -> loRaConfig.setSpreadingFactor(v));
 
-        seekBar_errorCoding = findViewById(R.id.seekBar_errorCoding);
-        new SeekBarHandler(seekBar_errorCoding, textView_errorCoding, LoRaConfig.errorCodingMap)
-                .setValueProxy(() -> loRaConfig.getErrorCoding(), (v) -> loRaConfig.setErrorCoding(v));
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_errorCoding),
+                findViewById(R.id.textView_errorCoding),
+                LoRaConfig.errorCodingMap
+        ).setValueProxy(() -> loRaConfig.getErrorCoding(), (v) -> loRaConfig.setErrorCoding(v));
+
+        new SeekBarHandler(
+                findViewById(R.id.seekBar_rxPacketTimeout),
+                findViewById(R.id.textView_rxPacketTimeout),
+                1, 65535, "ms"
+        ).setValueProxy(() -> loRaConfig.getRxPacketTimeout(), (v) -> loRaConfig.setRxPacketTimeout(v));
     }
 
     private void initSwitches() {
-        switch_crc = findViewById(R.id.switch_crc);
-    }
+        new SwitchHandler(
+                findViewById(R.id.switch_crc),
+                findViewById(R.id.textView_CRC),
+                loRaConfig.getCrc(),
+                (v) -> loRaConfig.setCrc(v)
+        );
 
-    private void syncTextViewsAndSwitches() {
-        switch_crc.setChecked(loRaConfig.getCrc() == 1);
-        switch_crc.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            this.loRaConfig.setCrc(isChecked ? 1 : 0);
-            textView_CRC.setText(isChecked ? "1" : "0");
-        });
+        new SwitchHandler(
+                findViewById(R.id.switch_implicitHeader),
+                findViewById(R.id.textView_implicitHeader),
+                loRaConfig.getImplicitHeaderOn(),
+                (v) -> loRaConfig.setImplicitHeaderOn(v)
+        ).setOnOffLabels("implicit", "explicit");
+
+        new SwitchHandler(
+                findViewById(R.id.switch_rxSingle),
+                findViewById(R.id.textView_rxSingle),
+                loRaConfig.getRxSingleOn(),
+                (v) -> loRaConfig.setRxSingleOn(v)
+        ).setOnOffLabels("single", "continue");
+
+        new SwitchHandler(
+                findViewById(R.id.switch_frequencyHop),
+                findViewById(R.id.textView_frequencyHop),
+                loRaConfig.getFrequencyHopOn(),
+                (v) -> loRaConfig.setFrequencyHopOn(v)
+        );
     }
 }
