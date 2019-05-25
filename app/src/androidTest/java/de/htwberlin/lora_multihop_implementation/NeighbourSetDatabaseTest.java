faf8248cb68f9893442fea6f50244d938941e882
peteme <p.meindl97@gmail.com>
2019-05-25 19:23:36 +0200

    added more test cases & two new DAO methods

--- app/src/androidTest/java/de/htwberlin/lora_multihop_implementation/NeighbourSetDatabaseTest.java
+++ app/src/androidTest/java/de/htwberlin/lora_multihop_implementation/NeighbourSetDatabaseTest.java
@@ -37,11 +37,111 @@ public class NeighbourSetDatabaseTest {
     }
 
     @Test
-    public void testAddDbEntry() {
-        neighbourSetDAO.saveNeighbourSet(createNeighbourSet());
-        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+    public void testSaveObjRemoveByObjSuccess() {
+        NeighbourSet ns = createNeighbourSetUid0000();
 
+        neighbourSetDAO.saveNeighbourSet(ns);
+        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
         Assert.assertEquals(1, neighbourSets.size());
+
+        neighbourSetDAO.deleteNeighbourSet(ns);
+        neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(0, neighbourSets.size());
+
+        neighbourSetDAO.clearTable();
+    }
+
+    @Test
+    public void testSaveObjRemoveByObjFail() {
+        NeighbourSet ns = createNeighbourSetUid0000();
+
+        neighbourSetDAO.saveNeighbourSet(ns);
+        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(1, neighbourSets.size());
+
+        ns = createNeighbourSetUid0001();
+        neighbourSetDAO.deleteNeighbourSet(ns);
+        neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(1, neighbourSets.size());
+
+        neighbourSetDAO.clearTable();
+    }
+
+    @Test
+    public void testSaveObjRemoveByUidSuccess() {
+        NeighbourSet ns = createNeighbourSetUid0000();
+
+        neighbourSetDAO.saveNeighbourSet(ns);
+        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(1, neighbourSets.size());
+
+        neighbourSetDAO.deleteNeighbourSetByUid(0000);
+        neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(0, neighbourSets.size());
+
+        neighbourSetDAO.clearTable();
+    }
+
+    @Test
+    public void testSaveObjRemoveObjByUidFail() {
+        NeighbourSet ns = createNeighbourSetUid0000();
+
+        neighbourSetDAO.saveNeighbourSet(ns);
+        List<NeighbourSet> neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(1, neighbourSets.size());
+
+        neighbourSetDAO.deleteNeighbourSetByUid(0001);
+        neighbourSets = neighbourSetDAO.getAllNeighbourSets();
+        Assert.assertEquals(1, neighbourSets.size());
+
+        neighbourSetDAO.clearTable();
+    }
+
+    @Test
+    public void testUpdateObjSuccess()  {
+        NeighbourSet ns = createNeighbourSetUid0000();
+        neighbourSetDAO.saveNeighbourSet(ns);
+
+        Assert.assertEquals("AAAB", ns.getDah());
+
+        ns.setDah("AAAX");
+        neighbourSetDAO.updateNeighbourSet(ns);
+        ns = neighbourSetDAO.getNeighbourSetByUid(0000);
+
+        Assert.assertEquals("AAAX", ns.getDah());
+    }
+
+    @Test
+    public void testSaveTwoObjAndClearTableSuccess()  {
+        NeighbourSet nsOne = createNeighbourSetUid0000();
+        NeighbourSet nsTwo = createNeighbourSetUid0001();
+
+        neighbourSetDAO.saveNeighbourSet(nsOne);
+        neighbourSetDAO.saveNeighbourSet(nsTwo);
+
+        Assert.assertEquals(2, neighbourSetDAO.getAllNeighbourSets().size());
+
+        neighbourSetDAO.clearTable();
+
+        Assert.assertEquals(0, neighbourSetDAO.getAllNeighbourSets().size());
+    }
+
+    @Test
+    public void testGetNsByUidSuccess() {
+        NeighbourSet nsForSave = createNeighbourSetUid0001();
+        neighbourSetDAO.saveNeighbourSet(nsForSave);
+
+        NeighbourSet nsFromDb = neighbourSetDAO.getNeighbourSetByUid(1);
+        Assert.assertEquals(nsForSave.getUid(), nsFromDb.getUid());
+    }
+
+    @Test
+    public void testGetNsByUidFail() {
+        NeighbourSet nsForSave = createNeighbourSetUid0001();
+        neighbourSetDAO.saveNeighbourSet(nsForSave);
+
+        NeighbourSet nsFromDb = neighbourSetDAO.getNeighbourSetByUid(2);
+        Assert.assertNull(nsFromDb);
     }
 
     @After
@@ -49,10 +149,17 @@ public class NeighbourSetDatabaseTest {
         db.close();
     }
 
-    private NeighbourSet createNeighbourSet()   {
+    private NeighbourSet createNeighbourSetUid0000()   {
         Location local = new Location("berlin");
         local.setLatitude(52.5200);
         local.setLongitude(13.4050);
-        return new NeighbourSet(0000, "AAAA", "AAAB", local, ELoraNodeState.UNKNOWN, new Timestamp(System.currentTimeMillis()));
+        return new NeighbourSet(0, "AAAA", "AAAB", local, ELoraNodeState.UNKNOWN, new Timestamp(System.currentTimeMillis()));
+    }
+
+    private NeighbourSet createNeighbourSetUid0001()   {
+        Location local = new Location("berlin");
+        local.setLatitude(52.5200);
+        local.setLongitude(13.4050);
+        return new NeighbourSet(1, "AAAB", "AAAC", local, ELoraNodeState.UNKNOWN, new Timestamp(System.currentTimeMillis()));
     }
 }
