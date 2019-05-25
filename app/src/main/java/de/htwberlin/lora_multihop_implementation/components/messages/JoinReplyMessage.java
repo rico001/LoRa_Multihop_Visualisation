0bf2619e9e4894cd36a1fe877c33b62b357a9bf9
s0561440 <tom-morelly@gmx.de>
2019-05-25 10:25:28 +0200

    - implemented all messages

--- app/src/main/java/de/htwberlin/lora_multihop_implementation/components/messages/JoinReplyMessage.java
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/messages/JoinReplyMessage.java
@@ -3,11 +3,29 @@ package de.htwberlin.lora_multihop_implementation.components.messages;
 import de.htwberlin.lora_multihop_implementation.enums.EMessageType;
 
 /**
+ * JOIN_REPLY ={SA, LAT, LONG}
+ *
+ * SA = Source Address
+ * LAT = Latitude
+ * Long = Longitude
+ *
+ * Predecessor Message = JOIN
+ * Reply Message = ACK
+ *
+ * Communication Type = UNICAST
+ *
+ * Use Case: Used to reply to JOIN messages.
+ *
  * @author morelly_t1
  */
 public class JoinReplyMessage extends JoinMessage {
 
+    private  static final EMessageType replyMessage = EMessageType.ACK;
+    private  static final boolean UNICAST = Boolean.TRUE;
+
+
     public JoinReplyMessage(EMessageType type, String sourceAddress, Double longitude, Double latitude) {
-        super(type, sourceAddress, longitude, latitude);
+        super(sourceAddress, longitude, latitude);
     }
+
 }
---
+++ app/src/main/java/de/htwberlin/lora_multihop_implementation/components/messages/Push.java
@@ -0,0 +1,74 @@
+package de.htwberlin.lora_multihop_implementation.components.messages;
+
+import de.htwberlin.lora_multihop_implementation.enums.EMessageType;
+
+/**
+ * PUSH ={SA;SAH; DAH; LAT; LONG;}
+ *
+ * SA = Source address
+ * SAH = Source Address of Hop
+ * DAH = Direct attached Hop
+ * LAT = Latitude
+ * LONG = Longitude
+ *
+ * Predecessor Message = Null
+ * Reply Message = ACK
+ *
+ * Communication Type = UNICAST
+ *
+ * Use Case: Used to send a NS entry to a destination hop
+ *
+ * @author morelly_t1
+ */
+
+public class Push extends Message {
+
+    private  static final EMessageType replyMessage = EMessageType.ACK;
+    private  static final boolean UNICAST = Boolean.TRUE;
+
+    private String sourceAddressOfHop;
+    private String directAttachedHop;
+    private double latitude, longitude;
+
+    public Push(String sourceAddress, String sourceAddressOfHop, String directAttachedHop, double latitude, double longitude) {
+        super(sourceAddress);
+        this.sourceAddressOfHop = sourceAddressOfHop;
+        this.directAttachedHop = directAttachedHop;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getSourceAddressOfHop() {
+        return sourceAddressOfHop;
+    }
+
+    public void setSourceAddressOfHop(String sourceAddressOfHop) {
+        this.sourceAddressOfHop = sourceAddressOfHop;
+    }
+
+    public String getDirectAttachedHop() {
+        return directAttachedHop;
+    }
+
+    public void setDirectAttachedHop(String directAttachedHop) {
+        this.directAttachedHop = directAttachedHop;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
+
+
