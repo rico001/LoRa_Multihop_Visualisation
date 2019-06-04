1494fc7fd0ffab4b8f5ec1c131abf86e20e94559
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2019-06-04 07:33:37 +0200

    impl UI element for favorite AT cmds

---
+++ app/src/main/java/de/htwberlin/lora_multihop_visualisation/LastCommandsUI_Layout.java
@@ -0,0 +1,78 @@
+package de.htwberlin.lora_multihop_visualisation;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import java.util.HashMap;
+
+import de.htwberlin.lora_multihop_implementation.interfaces.LoraCommandsConstants;
+
+/**
+ * Allows to save up to MAXSIZE favorite AT commands
+ * You can delete cmd with on long click on cmd button
+ */
+public class LastCommandsUI_Layout implements LoraCommandsConstants{
+
+    private static final int MAXSIZE=4;
+
+    /**
+     * contains favorite cmds
+     */
+    HashMap<String,String> lastCmds;
+
+    /**
+     * contains favorite cmds as buttons
+     */
+    LinearLayout lastComandsLayout;
+
+    Context context;
+
+    View.OnClickListener onClickListener;
+
+    public LastCommandsUI_Layout(LinearLayout lastComandsLayout, Context context, View.OnClickListener onItemClick){
+
+        lastCmds= new HashMap<>();
+
+        this.lastComandsLayout=lastComandsLayout;
+        this.context = context;
+        this.onClickListener=onItemClick;
+
+        addDefaultCommands();
+    }
+
+    public void addCMD(String newCMD){
+
+        if(lastCmds.containsKey(newCMD) || newCMD.isEmpty() || lastCmds.size()>=MAXSIZE)
+            return;
+
+        lastCmds.put(newCMD,newCMD);
+
+        update_LastCommandsUI_Layout(newCMD);
+    }
+
+    private void update_LastCommandsUI_Layout(String cmd){
+
+            Button b = new Button(context);
+            b.setOnClickListener(onClickListener);
+            b.setText(cmd);
+
+            b.setOnLongClickListener(view ->{
+                lastCmds.remove(cmd);
+                ((ViewGroup) view.getParent()).removeView(view);
+                return true;
+            });
+
+            lastComandsLayout.addView(b);
+    }
+
+    /**
+     * some default additions
+     */
+    private void addDefaultCommands(){
+        addCMD(TEST_CMD_MSG);
+        addCMD(GET_ADDRESS_CMD_MSG);
+        addCMD(SET_ADDRESS_CMD_MSG);
+    }
+}
--- app/src/main/java/de/htwberlin/lora_multihop_visualisation/TerminalFragment.java
+++ app/src/main/java/de/htwberlin/lora_multihop_visualisation/TerminalFragment.java
@@ -3,6 +3,7 @@ package de.htwberlin.lora_multihop_visualisation;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +29,7 @@ public class TerminalFragment extends Fragment {
 
     private ScrollView scrollView;
     private LinearLayout terminalMessages;
+    private LastCommandsUI_Layout lastComandsUILayout = null;
     private EditText terminalInput;
     private Button sendButton;
     private Button returnButton;
@@ -45,12 +47,23 @@ public class TerminalFragment extends Fragment {
         sendButton = (Button) view.findViewById(R.id.terminal_send_button);
         returnButton = (Button) view.findViewById(R.id.return_to_map);
 
+        lastComandsUILayout = new LastCommandsUI_Layout(view.findViewById(R.id.linearLayout_lastCommands), getContext(), v-> {
+            Button b = (Button) v;
+            terminalInput.setText(b.getText());
+        });
+
+
+
+
         /**
          * Action for the send button
          */
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                lastComandsUILayout.addCMD(terminalInput.getText().toString());
+
                 if (v.getId() == sendButton.getId()) {
                     try {
                         ((MainActivity) getActivity()).btService.write((terminalInput.getText().toString() + AT_POSTFIX).getBytes());
